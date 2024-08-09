26ac1d692996eb0f706b48cda23f89ea2cf5acad
mihai ionescu <d.mihai.ionescu@gmail.com>
2024-08-09 11:55:09 +0300

    update demo test that I forgot to commit yesterday

--- tests/Feature/TasksControllerTest.php
+++ tests/Feature/TasksControllerTest.php
@@ -12,31 +12,20 @@ class TasksControllerTest extends TestCase
     use RefreshDatabase, WithoutMiddleware, CreatesApplication;
 
 
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/api/');
-
-        $response->assertStatus(200);
-    }
-
-
     public function testStoreMethod()
     {
         $taskData = [
-            'title' => 'Study for test',
+            'name' => 'Study for test',
             'description' => 'Start studying for mathematics test',
-            'status' => 'In progress',
-            'due_date' => '2022-12-31',
         ];
 
-        $response = $this->json('POST', '/api/tasks', $taskData);
+        $response = $this->json('POST', '/api/todos', $taskData);
 
         // Ensure that the HTTP status code is 201
         $response->assertStatus(201);
 
         // Check returned data
-        $response->assertJsonFragment(['message' => 'Task created successfully']);
-        $response->assertJsonPath('data.title', $taskData['title']);
+        $response->assertJsonPath('name', $taskData['name']);
 
         // Verify that the task is successfully stored in the database
         $this->assertDatabaseHas('tasks', $taskData);
